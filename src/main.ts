7d717950b311ced39f360c14bb50e79262f19446
Justin Shilling <loveofmusic25@hotmail.com>
2023-08-23 21:43:29 -0400

    add new packages, config adjustments

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import router from "./router";
+import router from "@/router";
+import pinia from "@/stores";
+import { Quasar } from "quasar";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import {
@@ -10,10 +12,13 @@ import {
   faBars,
   faCircle,
 } from "@fortawesome/free-solid-svg-icons";
+import "quasar/dist/quasar.css";
 
 library.add(faPencil, faTrash, faTimes, faBars, faCircle);
 
 const app = createApp(App);
 app.component("font-awesome-icon", FontAwesomeIcon);
 app.use(router);
+app.use(pinia);
+app.use(Quasar, {});
 app.mount("#app");
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,26 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { quasar, transformAssetUrls } from "@quasar/vite-plugin";
+import { fileURLToPath } from "url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue({
+      template: { transformAssetUrls },
+    }),
+    quasar(),
+  ],
   define: {
-    "process.env": process.env,
+    "process.env": {},
+  },
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+    extensions: [".js", ".json", ".ts", ".vue"],
+  },
+  css: {
+    devSourcemap: true,
   },
 });
