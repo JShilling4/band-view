6cd4b0031d61ae2b84c3f6a881dbbd19c55c4c89
Justin Shilling <loveofmusic25@hotmail.com>
2024-01-20 15:13:52 -0500

    supabase config adjustment

--- src/supabase/index.ts
+++ src/supabase/index.ts
@@ -2,6 +2,6 @@ import { createClient } from "@supabase/supabase-js";
 import { Database } from "@/types";
 
 export default createClient<Database>(
-  "https://sohrxoilutntychfxjrq.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNvaHJ4b2lsdXRudHljaGZ4anJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTIyOTAyNDMsImV4cCI6MjAwNzg2NjI0M30.rGUKQH9H73ynjWLgv7ZQxlWPzHR9KvJoQIUV3ccMjZY"
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
 );
