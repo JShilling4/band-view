5d7a302db0b6ad25064e6c0f72aba7b7d1899a00
Justin Shilling <loveofmusic25@hotmail.com>
2024-02-11 20:49:29 -0500

    fixes side nav shiftying on mobile

--- src/components/Navigation/SideNavigation.vue
+++ src/components/Navigation/SideNavigation.vue
@@ -1,14 +1,41 @@
 <template>
   <AppDrawer
     v-model="leftDrawerOpen"
-    :mini="miniLeftDrawer"
+    :mini="$q.screen.width < 1024 && miniLeftDrawer"
     side="left"
     :width="$q.screen.width > 1024 ? 250 : 45"
-    class="app-left-drawer bg-grey-10 text-grey-3"
+    class="app-left-drawer bg-black text-grey-3"
     behavior="desktop"
     show-if-above
     persistent
+    :mini-width="45"
   >
+    <template #mini>
+      <QScrollArea class="fit" :horizontal-thumb-style="{ opacity: '0' }">
+        <QList>
+          <QItem
+            v-for="(item, i) in navConfig"
+            :key="i"
+            :active="$route.name === item.routeName"
+            active-class="active-route"
+            clickable
+            v-ripple
+            :to="item.routePath"
+            :href="item.link?.href"
+            :target="item.link?.target"
+          >
+            <QItemSection avatar>
+              <QIcon
+                :name="item.icon.classes.join(' ')"
+                :color="item.icon.color"
+                class="mini-icon"
+              />
+            </QItemSection>
+            <!-- <QItemSection> {{ item.label }} </QItemSection> -->
+          </QItem>
+        </QList>
+      </QScrollArea>
+    </template>
     <QScrollArea class="fit" :horizontal-thumb-style="{ opacity: '0' }">
       <QList>
         <QItem
@@ -42,7 +69,7 @@ const $q = useQuasar();
 const $route = useRoute();
 
 const leftDrawerOpen = ref(false);
-const miniLeftDrawer = ref(false);
+const miniLeftDrawer = ref(true);
 </script>
 
 <style lang="sass" scoped>
