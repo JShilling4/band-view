eff5f0fb0765a867facf5ebf774c32541742491d
Justin Shilling <loveofmusic25@hotmail.com>
2025-02-22 18:26:17 -0500

    RehearsalItem structural improvements

---
+++ src/components/RehearsalItem/RehearsalItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="rehearsal-item">
+    <SongSection title="New Songs" :songs="rehearsal.new_songs" />
+
+    <SongSection title="Review Songs" :songs="rehearsal.review_songs" hide-admin />
+
+    <div class="agenda-container">
+      <QSeparator color="grey-4" spaced />
+      <div class="section-header text-grey-6">Notes:</div>
+      <QSeparator color="grey-4" spaced />
+      <p class="rehearsal-notes">{{ rehearsal.notes || "No notes added." }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Tables } from "@/types";
+
+defineProps<{
+  rehearsal: Tables<"rehearsal">;
+}>();
+</script>
+
+<style lang="scss" scoped>
+.rehearsal-notes {
+  margin: 1rem 0;
+  white-space: pre-wrap;
+}
+</style>
